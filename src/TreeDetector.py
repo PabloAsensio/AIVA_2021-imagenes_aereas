df484ed6fc0b3533f6c83a9e289c37c1592d3f01
pasensio97 <p.asensio97@gmail.com>
2021-03-21 14:28:43 +0100

    Update TreeDetector class #2

--- src/TreeDetector.py
+++ src/TreeDetector.py
@@ -1,6 +1,9 @@
+import cv2 as cv
+
+
 class TreeDetector:
     def __init__(self, img):
-        self.__img = img
+        self.__img = cv.imread(img)
 
     def recognize(self):
         return "done"
